c5db7b85e1d97bea8fd0be8f3be573885800a483
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-10 18:09:02 +0100

    up

--- index.js
+++ index.js
@@ -1,67 +1,138 @@
-const search = "cars";
+let imageLibrary = [];
+let imageLibrarySecondary = [];
+let imageLibraryForest = [];
+let allViewButtons = [];
+let allEditButtons = [];
+let imageSet_One;
 
-const url = "http://www.splashbase.co/api/v1/images/search?query=" + search;
+function successAlert(numberOfImages) {
+  const alert = document.querySelector(".alert-success");
+  alert.classList.remove("d-none");
+  alert.classList.remove("slide-out-top");
+  alert.classList.add("d-block");
+  alert.classList.add("swing-in-top-fwd");
+  alert.innerText = `Successfully loaded ${numberOfImages} Images!`;
 
-// generating modals
-const generateModals = ({ images }) => {
-  images.forEach((element) => {
-    console.log(element);
+  setTimeout(function () {
+    alert.classList.remove("d-block");
+    alert.classList.add("slide-out-top");
+  }, 3000);
+}
+
+function populateCarousel() {
+  const carouselContainer = document.querySelector(".carousel-inner");
+  for (let i = 0; i < 8; i++) {
+    const newItem = document.createElement("div");
+    newItem.classList.add("carousel-item");
+    newItem.innerHTML = `        <img
+          src="${imageLibraryForest[0][i].url}"
+          class="d-block w-100"
+          alt="..."
+        />`;
+    carouselContainer.appendChild(newItem);
+  }
+  const firstCarouselItem = document.querySelector(".carousel-item");
+  firstCarouselItem.classList.add("active");
+}
+
+async function loadCarouselImages() {
+  const response = await fetch(
+    "http://www.splashbase.co/api/v1/images/search?query=forest"
+  ).then(async (response) => {
+    let data = await response.json();
+    imageLibraryForest.push(data.images);
+    console.log("Loaded images");
   });
-};
-//
 
-const fetchImages = () => {
-  fetch(url)
-    .then((res) => res.json())
-    .then(loadImages)
-    .then(generateModals)
-    .catch((err) => console.log(err));
-};
-function loadImages({ images }) {
-  let cards = document.querySelectorAll(".card");
-  for (let i = 0; i < cards.length; i++) {
-    let list = cards[i];
-
-    list.firstElementChild.remove();
-    let imgs = document.createElement("img");
-    imgs.classList.add("img-fluid");
-    imgs.style.height = "250px";
-    imgs.src = images[i].url;
-    list.innerHTML = imgs.outerHTML + " " + list.innerHTML;
-  }
-  hideCards();
-}
-const search2 = "trees";
-
-const url2 = "http://www.splashbase.co/api/v1/images/search?query=" + search2;
-
-const fetchImages2 = () => {
-  fetch(url2)
-    .then((res) => res.json())
-    .then(loadImages2)
-    .catch((err) => console.log(err));
-};
-const cards = document.querySelectorAll(".card");
-function loadImages2({ images }) {
-  for (let i = 0; i < cards.length; i++) {
-    let list = cards[i];
-    list.firstElementChild.remove();
-    let imgs = document.createElement("img");
-    imgs.classList.add("img-fluid");
-    imgs.style.height = "250px";
-    imgs.src = images[i].url;
-    list.innerHTML = imgs.outerHTML + " " + list.innerHTML;
-  }
-  hideCards();
+  populateCarousel();
+  successAlert("Carousel");
 }
 
-const hideCards = () => {
-  let btn = document.querySelectorAll("#hide");
-  btn.forEach((element) => {
-    element.addEventListener("click", function hidestuff(e) {
-      const targetCard =
-        e.currentTarget.parentNode.parentNode.parentNode.parentNode;
-      targetCard.style.display = "none";
+async function loadImage() {
+  const response = await fetch(
+    `http://www.splashbase.co/api/v1/images/latest`
+  ).then(async (response) => {
+    let data = await response.json();
+    imageLibrary.push(data);
+  });
+
+  const allImgIDs = document.querySelectorAll("small");
+  for (let i = 0; i < allImgIDs.length; i++) {
+    allImgIDs[i].innerText = imageLibrary[0].images[i].id;
+  }
+
+  imageSet_One = true;
+  successAlert(10);
+}
+
+async function loadImagesSecondary() {
+  for (let i = 0; imageLibrarySecondary.length < 10; i++) {
+    const response = await fetch(
+      `http://www.splashbase.co/api/v1/images/random`
+    ).then(async (response) => {
+      let data = await response.json();
+      imageLibrarySecondary.push(data);
     });
+  }
+  const allImgIDs = document.querySelectorAll("small");
+  for (let i = 0; i < allImgIDs.length; i++) {
+    allImgIDs[i].innerText = imageLibrarySecondary[i].id;
+  }
+  imageSet_One = false;
+  successAlert(10);
+}
+
+function openModal() {
+  const target = event.currentTarget;
+
+  for (let i = 0; i < allViewButtons.length; i++) {
+    if (imageSet_One === true) {
+      if (target === allViewButtons[i]) {
+        const modalBody = document.querySelector(".modal-body");
+        modalBody.innerHTML = `<img class="w-100" src="${imageLibrary[0].images[i].url}" />`;
+      }
+    } else {
+      if (target === allViewButtons[i]) {
+        const modalBody = document.querySelector(".modal-body");
+        modalBody.innerHTML = `<img class="w-100" src="${imageLibrarySecondary[i].url}" />`;
+        console.log("second set");
+      }
+    }
+  }
+}
+
+function hideCard() {
+  const targetCard =
+    event.currentTarget.parentNode.parentNode.parentNode.parentNode;
+  targetCard.style.display = "none";
+}
+
+function assignButtonIds() {
+  const allButtons = document.querySelectorAll(".btn-outline-secondary");
+
+  for (let i = 0; i < allButtons.length; i++) {
+    if (i % 2 === 0) {
+      allButtons[i].addEventListener("click", openModal);
+      allButtons[i].setAttribute("data-toggle", "modal");
+      allButtons[i].setAttribute("data-target", "#exampleModal");
+      allViewButtons.push(allButtons[i]);
+    } else {
+      allButtons[i].addEventListener("click", hideCard);
+      allEditButtons.push(allButtons[i]);
+    }
+  }
+}
+
+function replaceEditButtons() {
+  allEditButtons.forEach((e) => {
+    e.innerText = "Hide";
   });
-};
+}
+
+function start() {
+  assignButtonIds();
+  replaceEditButtons();
+  loadCarouselImages();
+}
+
+window.onload = start();
