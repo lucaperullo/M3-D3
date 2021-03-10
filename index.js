8552aa9aa76c1cf8469b03cfc76e5fc90eba3598
Mihai Ivanov <mikee2988@yahoo.com>
2021-03-10 16:02:01 +0200

    update first link

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-// const search =
+const search = "cars";
 
-// const url = "http://www.splashbase.co/api/v1/images/search?query=" + search
+const url = "http://www.splashbase.co/api/v1/images/search?query=" + search;
+
+const fetchImages = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then(loadImages)
+    .catch((err) => console.log(err));
+};
+function loadImages({ images }) {
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cards.length; i++) {
+    let list = cards[i];
+    list.firstElementChild.remove();
+    let imgs = document.createElement("img");
+    imgs.classList.add("img-fluid");
+    imgs.style.height = "250px";
+    imgs.src = images[i].url;
+    list.innerHTML = imgs.outerHTML + " " + list.innerHTML;
+  }
+}
